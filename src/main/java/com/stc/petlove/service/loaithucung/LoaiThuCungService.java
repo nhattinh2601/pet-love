01482edca0564cc66a82e85b9907c39f6b83d999
nhattinh2601 <nhattinh2601@gmail.com>
2023-04-18 16:43:06 +0700

    up

---
+++ src/main/java/com/stc/petlove/service/loaithucung/LoaiThuCungService.java
@@ -0,0 +1,22 @@
+package com.stc.petlove.service.loaithucung;
+
+
+import com.stc.petlove.dtos.loaithucung.LoaiThuCungDto;
+import com.stc.petlove.entities.LoaiThuCung;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface LoaiThuCungService {
+    Page<LoaiThuCung> filter(String search, int page, int size, String sort, String column);
+    LoaiThuCung getLoaiThuCungByMaLoaiThuCung(String maLoaiThuCung);
+    List<LoaiThuCung> getAllLoaiThuCung(String search);
+    List<LoaiThuCung> finfAll();
+
+    LoaiThuCung create(LoaiThuCungDto dto);
+
+    LoaiThuCung update(String id, LoaiThuCungDto dto);
+
+
+    LoaiThuCung changeStatus(String maLoaiThuCung);
+}
